87e7fe8040b5ceb780bb859c38a35f1d6821a6db
Derrick Kwaku Adonteng <138382322+dkadonteng-developer@users.noreply.github.com>
2023-12-15 04:51:15 +0000

    Update guessgame.py

--- guessgame.py
+++ guessgame.py
@@ -1,9 +1,7 @@
 import random
 
-# Generate a random number between 1 and 100
 secret_number = random.randint(1, 100)
 
-# Set the initial number of attempts
 attempts = 0
 
 print("Welcome to the Number Guessing Game!")
@@ -11,11 +9,9 @@ print("I'm thinking of a number between 1 and 100.")
 
 while True:
     try:
-        # Ask the player to guess the number
         guess = int(input("Guess the number: "))
         attempts += 1
 
-        # Check if the guess is correct
         if guess == secret_number:
             print(f"Congratulations! You guessed the number {secret_number} in {attempts} attempts.")
             break
@@ -26,4 +22,3 @@ while True:
     except ValueError:
         print("Please enter a valid number.")
 
-# End of the game
